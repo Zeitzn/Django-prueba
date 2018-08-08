72c65e65bfc6a3afc4728e66f352b3aa6c37a5fe
Isac <smevy4lc3@gmail.com>
2018-08-08 12:36:44 -0500

    Modelos 2

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Editor(models.Model):
+    nombre=models.CharField(max_length=30)
+    domicilio=models.CharField(max_length=50)
+    ciudad=models.CharField(max_length=60)
+    estado=models.CharField(max_length=30)
+    pais=models.CharField(max_length=50)
+    website=models.URLField()
+
+class Autor(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=40)
+    email=models.EmailField()
+
+class Libro(models.Model):
+    titulo=models.CharField(max_length=100)
+    autores=models.ManyToManyField(Autor)
+    editor=models.ForeignKey(Editor, null=True,blank=True, on_delete=models.CASCADE)
+    fecha_publicacion=models.DateField()
+    portada=models.CharField(max_length=100)
